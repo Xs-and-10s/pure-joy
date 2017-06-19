50fa31459033ed136f01e5b75836867326dba616
Mark Manley <mark.manley.2010@gmail.com>
2017-06-19 10:41:51 -0400

    make components prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,45 @@
 // @flow
 
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import type { Match } from 'react-router-dom';
-import AsyncRoute from './AsyncRoute';
-import store from './store';
-import preload from '../data.json';
+import React from "react";
+import { Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import type { Match } from "react-router-dom";
+import AsyncRoute from "./AsyncRoute";
+import store from "./store";
+import preload from "../public/data.json";
 
-const App = () => (
+const App = () =>
   <Provider store={store}>
     <div className="app">
-      <Route exact path="/" component={props => <AsyncRoute props={props} loadingPromise={import('./Landing')} />} />
+      <Route
+        exact
+        path="/"
+        component={props =>
+          <AsyncRoute props={props} loadingPromise={import("./Landing")} />}
+      />
       <Route
         path="/search"
-        component={props => (
-          <AsyncRoute loadingPromise={import('./Search')} props={Object.assign({ shows: preload.shows }, props)} />
-        )}
+        component={props =>
+          <AsyncRoute
+            loadingPromise={import("./Search")}
+            props={Object.assign({ shows: preload.shows }, props)}
+          />}
       />
       <Route
         path="/details/:id"
         component={(props: { match: Match }) => {
-          const selectedShow = preload.shows.find((show: Show) => props.match.params.id === show.imdbID);
+          const selectedShow = preload.shows.find(
+            (show: Show) => props.match.params.id === show.imdbID
+          );
           return (
             <AsyncRoute
-              loadingPromise={import('./Details')}
+              loadingPromise={import("./Details")}
               props={Object.assign({ show: selectedShow, match: {} }, props)}
             />
           );
         }}
       />
     </div>
-  </Provider>
-);
+  </Provider>;
 
 export default App;
